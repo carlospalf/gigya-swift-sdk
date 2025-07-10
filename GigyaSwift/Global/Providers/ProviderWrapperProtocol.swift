e5ac011d5203f0854b57cb604f55af2d8e0b64f5
carlospalf <carlos.parada@tailor-hub.com>
2025-07-10 10:51:16 +0200

    Revert "Expose ProviderWrapperProtocol in umbrella header"
    This reverts commit 68b33ef3a7d02a898e568a84f114c1273d383e5d.

--- GigyaSwift/Global/Providers/ProviderWrapperProtocol.swift
+++ GigyaSwift/Global/Providers/ProviderWrapperProtocol.swift
@@ -1,11 +1,21 @@
+//
+//  ProviderWrapperProtocol.swift
+//  GigyaSwift
+//
+//  Created by Shmuel, Sagi on 23/04/2019.
+//  Copyright © 2019 Gigya. All rights reserved.
+//
+
+import UIKit
+
 @objc public protocol ProviderWrapperProtocol {
     init()
-
+    
     var clientID: String? { get set }
-
-    func login(_ params: NSDictionary?,
-                         viewController: UIViewController?,
-                         completion: @escaping (NSDictionary?, NSString?) -> Void)
+    
+    func login(params: [String: Any]?, viewController: UIViewController?,
+               completion: @escaping (_ jsonData: [String: Any]?, _ error: String?) -> Void)
 
     @objc optional func logout()
+
 }
